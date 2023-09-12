6d286dcdc00db7da3762a75b2c6ff287d758b499
opxzy22 <aopeyem84@gmail.com>
2023-09-11 18:29:43 -0700

    progress

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const arg_number = process.argv.length;
-if (arg_number === 0) {
+if (arg_number === 2) {
   console.log('No argument');
-} else if (arg_number === 1) {
+} else if (arg_number === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const firs_arg = process.argv[2];
+const first_arg = process.argv[2];
 
 if first_arg.length == 'undefined' {
 	console.log('No argument');
-}
-else {
+} else {
 	console.log(first_arg);
 }
