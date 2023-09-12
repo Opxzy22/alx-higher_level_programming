db77833462d7467d1c598e315b3d87d584ee1724
opxzy22 <aopeyem84@gmail.com>
2023-09-12 08:30:10 -0700

    progress

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-arg1 = process.argv[2];
-arg2 = process.argv[3];
+let arg1 = process.argv[2];
+let arg2 = process.argv[3];
 
-Args = `${arg1} is ${arg2}`;
+let Args = `${arg1} is ${arg2}`;
 
 console.log(Args);
