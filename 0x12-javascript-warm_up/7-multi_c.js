e13743514c43f022137dc42520d0701ae74b2ee3
opxzy22 <aopeyem84@gmail.com>
2023-09-12 07:04:04 -0700

    workings

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+arg = process.argv[2];
+
+if (isNaN(arg)) {
+	console.log('Missing number of occurrences');
+}
+else
+	for (let x = 0; x < arg; x++) {
+		console.log('C is fun');
+}
