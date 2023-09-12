c2c54a5ba3c91f66caa08e162c9dde3c2bab8898
opxzy22 <aopeyem84@gmail.com>
2023-09-11 17:33:28 -0700

    workings

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args_number = process.argv[2]
+if length(args) == 0 {
+	console.log('No argument');
+} else length(args) == 1 {
+	console.log('Argument found');
+} else {
+	console.log('Argument found')
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const first_arg = process.argv[2];
+
+if first_arg.length == 'undefined' {
+	console.log('No argument');
+}
