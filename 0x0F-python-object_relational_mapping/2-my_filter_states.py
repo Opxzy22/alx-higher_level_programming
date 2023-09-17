88c49ad9bd564362d3bb067656e8c00cc434224b
opxzy22 <aopeyem84@gmail.com>
2023-09-17 06:02:18 -0700

    work

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    usernamee = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306, user=username, passwd=password, db=database)
+    c = db.cursor()
+
+    query = "SELECT * states WHERE name LIKE BINARY = s% ORDER BY ASC"
+    c.execute(query, (state_name))
+
+    result = c.fetchall()
+
+    for arg in result:
+        print(arg)
+
+        c.close()
+        db.close()
