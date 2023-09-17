7b604289a16378c47aedad1ee0194470cafee273
opxzy22 <aopeyem84@gmail.com>
2023-09-17 01:03:31 -0700

    list all states that start with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-import sys
-import MySQLdb
+    import sys
+    import MySQLdb
 
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-db = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="local host",
         port=3306,
         user=username,
         passwd=password,
         db=database)
 
-c = db.cursor()
+    c = db.cursor()
 
-c.execute("select * FROM states WHERE name BINARY LIKE 'N%' ORDER BY id ASC")
-Nstate = c.fetchall()
+    c.execute("SELECT * FROM states WHERE name BINARY LIKE 'N%' ORDER BY id ASC")
+    Nstate = c.fetchall()
 
-for state in Nstate:
-    print(state)
+    for state in Nstate:
+        print(state)
