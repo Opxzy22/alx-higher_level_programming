f68da07e7d1d0f30d6611c09e64b7c65f2434aea
opxzy22 <aopeyem84@gmail.com>
2023-09-17 21:40:01 -0700

    work

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Create the database engine
+    """
+    engine = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    """
+    create a session to interact with the engine
+    """
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    del_name = session.query(State).filter(State.name.like("%a%").all()
+    for state in del_name:
+        session.delete(state)
+
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Create the database engine
+    """
+    engine = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    """
+    create a session to interact with the engine
+    """
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    a_states = session.query(State).filter(State.name.like(
+        '%a%')).order_by(State.id).all()
+    for states in a_states:
+        print(states.name, states.id)
